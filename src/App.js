cbf64b479a3129c789646452059da3cc56003c7f
marcos medina <marcosjmedina@gmail.com>
2022-01-17 16:42:33 -0300

    estructura de footer creada,imagen de logo importada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './Components/Footer';
+
+
 
 function App() {
   return (
     <div>
       <h1>prueba</h1>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Container,Row,Col } from 'react-bootstrap';
+import logoRollingvet from '../Components/img/logoRollingvet.png'
+
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <Container fluid className='footerContainer'>
+        <Row>
+          <Col sm={4} className='textoFooter text-center'> <img src={logoRollingvet} alt='logo' className='logoRollingvet' /> </Col>
+          <Col sm={4} className='textoFooter'>CONTACTANOS!
+          <div>
+              <ul>
+                  <li>+54 (381) 642-6143 </li>
+                  <li>CASA CENTRAL:(381) 642-6143 / BARRIO SUR:(381) 327-9474</li>
+                  <li>rollingvet@gmail.com</li>
+                  <li>Av.Alem 532(Casa Central) / Jujuy 572(Barrio Sur)</li>
+              </ul>
+          </div></Col>
+          <Col sm={4} className='textoFooter'>SIGAMOS CONECTADOS</Col>
+        </Row>
+      </Container>
     );
 };
 
