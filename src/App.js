0de6a7cefa19c31d81a8965ad1654c3936dc79a1
marcos medina <marcosjmedina@gmail.com>
2022-01-17 21:56:32 -0300

    footer maquetado. Libreria font asewome agregada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Footer from './Components/Footer';
-
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Components/Footer";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBell} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Righteous&family=Roboto+Mono&display=swap');
+      </style>
       <h1>prueba</h1>
+      <FontAwesomeIcon icon={faBell}/>
       <Footer></Footer>
     </div>
   );
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import { Container,Row,Col } from 'react-bootstrap';
-import logoRollingvet from '../Components/img/logoRollingvet.png'
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import logoRollingvet from "../Components/img/logoRollingvet.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWhatsapp,
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faEnvelope, faHome } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-    return (
-        <Container fluid className='footerContainer'>
-        <Row>
-          <Col sm={4} className='textoFooter text-center'> <img src={logoRollingvet} alt='logo' className='logoRollingvet' /> </Col>
-          <Col sm={4} className='textoFooter'>CONTACTANOS!
+  return (
+    <Container fluid className="footerContainer">
+      <Row>
+        <Col sm={2} className="textoFooter text-center py-4">
+          {" "}
+          <img
+            src={logoRollingvet}
+            alt="logo"
+            className="logoRollingvet"
+          />{" "}
+        </Col>
+        <Col sm={7} className="textoFooter py-3"> <h3 className="text-center">CONTACTANOS!</h3>
           <div>
-              <ul>
-                  <li>+54 (381) 642-6143 </li>
-                  <li>CASA CENTRAL:(381) 642-6143 / BARRIO SUR:(381) 327-9474</li>
-                  <li>rollingvet@gmail.com</li>
-                  <li>Av.Alem 532(Casa Central) / Jujuy 572(Barrio Sur)</li>
-              </ul>
-          </div></Col>
-          <Col sm={4} className='textoFooter'>SIGAMOS CONECTADOS</Col>
-        </Row>
-      </Container>
-    );
+            <ul className="py-4 listaContactos">
+              <li className="py-1">
+                {" "}
+                <FontAwesomeIcon
+                  icon={faWhatsapp}
+                  className="iconoContacto"
+                  style={{ fontSize: "2em" }}
+                />
+                +54 (381) 642-6143{" "}
+              </li>
+              <li className="py-1">
+                {" "}
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  className="iconoContacto"
+                  style={{ fontSize: "2em" }}
+                />
+                CASA CENTRAL:(381) 642-6143 / BARRIO SUR:(381) 327-9474
+              </li>
+              <li className="py-1">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="iconoContacto"
+                  style={{ fontSize: "2em" }}
+                />
+                rollingvet@gmail.com
+              </li>
+              <li className="py-1">
+                <FontAwesomeIcon
+                  icon={faHome}
+                  className="iconoContacto"
+                  style={{ fontSize: "2em" }}
+                />
+                Av.Alem 532(Casa Central) / Jujuy 572(Barrio Sur)
+              </li>
+            </ul>
+          </div>
+        </Col>
+        <Col sm={3} className="textoFooter py-3 text-center">
+          <h3 className="Text-center">SEGUINOS!</h3>
+          <div>
+            <ul className="py-5 iconosFooter">
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className="iconoRedes"
+                style={{ fontSize: "4em" }}
+              />
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="iconoRedes"
+                style={{ fontSize: "4em" }}
+              />
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className="iconoRedes"
+                style={{ fontSize: "4em" }}
+              />
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Footer;
