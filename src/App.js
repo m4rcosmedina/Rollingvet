2d36a7aa6dbbaebd9b79006c8b8590301c6462e3
marcos medina <marcosjmedina@gmail.com>
2022-01-20 17:48:29 -0300

    creado comp ListaTunos, y ruta asiganada

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Inicio from "./Components/Inicio";
 import Error404 from "./Components/Error404";
+import ListaTurnos from "./Components/ListaTurnos";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route exact path='/' element={<Inicio></Inicio>}></Route>
-          <Route exact path='*' element={<Error404></Error404>}></Route>
+          <Route exact path="/" element={<Inicio></Inicio>}></Route>
+          <Route exact path="*" element={<Error404></Error404>}></Route>
+          <Route
+            exact
+            path="/ListaTurnos"
+            element={<ListaTurnos></ListaTurnos>}
+          ></Route>
         </Routes>
       </Router>
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Righteous&family=Roboto+Mono&display=swap');
       </style>
-
     </div>
   );
 }
---
+++ src/Components/ListaTurnos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListaTurnos = () => {
+    return (
+        <div>
+            <h1>Hola ROba</h1>
+        </div>
+    );
+};
+
+export default ListaTurnos;
