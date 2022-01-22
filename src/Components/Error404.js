d0961a7f265062e2e260be38ae63016309f57e22
marcos medina <marcosjmedina@gmail.com>
2022-01-22 13:54:19 -0300

    pagina 404 creada,con imagen y pagina de estilos

--- src/Components/Error404.js
+++ src/Components/Error404.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "../Error404.css";
+import logoHorizontal from '../Components/img/logoHorizontal.png';
+
 
 const Error404 = () => {
-    return (
-        <div>
-            <h1>ERROR 404</h1>
-        </div>
-    );
+  return (
+    <div className="divSuperior">
+      <div class="error">404</div>
+      <br />
+      <br />
+      <span class="info">File not found <img src={logoHorizontal}
+      className="logoHorizontal"
+      alt="logoHorizontal"/></span>
+      
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Righteous&family=Roboto+Mono&family=VT323&display=swap');
+      </style>
+    </div>
+  );
 };
 
 export default Error404;
