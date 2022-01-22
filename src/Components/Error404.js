8ed63dafe1d6d907c06b30418b79e04b66cc9440
marcos medina <marcosjmedina@gmail.com>
2022-01-22 20:40:50 -0300

    pagina 404 modificada completa

--- src/Components/Error404.js
+++ src/Components/Error404.js
@@ -2,19 +2,29 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "../Error404.css";
-import logoHorizontal from '../Components/img/logoHorizontal.png';
-
+import logoHorizontal from "../Components/img/logoHorizontal.png";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Error404 = () => {
   return (
-    <div className="divSuperior">
-      <div class="error">404</div>
-      <br />
-      <br />
-      <span class="info">File not found <img src={logoHorizontal}
-      className="logoHorizontal"
-      alt="logoHorizontal"/></span>
-      
+    <div>
+      <Container fluid>
+        <Row>
+          <Col sm={6} className="ladoIzq text-center">
+            
+              <img
+                src={logoHorizontal}
+                className="logoHorizontal"
+                alt="logoHorizontal"/>
+            
+          </Col >
+          <Col sm={6} className="ladoDer py-4">
+            <p className="text-center numero404"> 404</p>
+            <p className="text-center texto404">Página no encontrada</p>
+          </Col>
+        </Row>
+      </Container>
+
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Righteous&family=Roboto+Mono&family=VT323&display=swap');
