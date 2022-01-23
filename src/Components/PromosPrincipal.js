c2fe1a8674057943acdecac92e6cfb79b7728de2
marcos medina <marcosjmedina@gmail.com>
2022-01-23 20:17:05 -0300

    cards de promo con estilo, imagen de prueba funcionando

--- src/Components/PromosPrincipal.js
+++ src/Components/PromosPrincipal.js
@@ -1,67 +1,70 @@
-import React from 'react';
-import { Card, CardGroup } from 'react-bootstrap';
+import React from "react";
+import { Card, CardGroup, Container } from "react-bootstrap";
+import pizza from "../assets/img/pizza.jpg";
+import "../CSS/App.css";
 
 const PromosPrincipal = () => {
-    return (
-        <div>
-            <CardGroup>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Container fluid className="my-4 ">
+      <CardGroup className="fondoGeneral">
+        <Card className="cardsPromo mx-4">
+          <Card.Img variant="top" src={pizza} />
+          <Card.Body>
+            <Card.Title>PROMO 1</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card className="cardsPromo mx-4">
+          <Card.Img variant="top" src={pizza} />
+          <Card.Body>
+            <Card.Title>PROMO 2</Card.Title>
+            <Card.Text>
+              lorem10
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card >
+        <Card className="cardsPromo mx-4">
+          <Card.Img variant="top" src={pizza} />
+          <Card.Body>
+            <Card.Title>PROMO 3</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card >
+        <Card className="cardsPromo mx-4">
+          <Card.Img variant="top" src={pizza} />
+          <Card.Body>
+            <Card.Title>PROMO 4</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      </Container>
+    </div>
+  );
 };
 
 export default PromosPrincipal;
