431aedd34ef34ba65927826573fe026802975615
Julieta <julietahurtadocabbad@gmail.com>
2022-01-25 02:43:14 -0300

    se le agregaron detalles al DatePicker

--- src/pages/ListaTurnos.js
+++ src/pages/ListaTurnos.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Container, Button } from 'react-bootstrap';
-import DatePicker from "react-datepicker"
+import DatePicker from "react-datepicker";
 import TimePicker from 'react-time-picker';
+import es from 'date-fns/locale/es';
+import "react-datepicker/dist/react-datepicker.css";
+import "react-time-picker/dist/TimePicker.css";
+import "react-clock/dist/Clock.css";
 
 const ListaTurnos = () => {
+    const [selectedDate, setSelectedDate] = useState(null)
     return (
         <div className="fondoGeneral">
-            <h1 className='text-center'>SOLICITE SU TURNO</h1>
+            <h1 className='text-center py-4'>TURNOS</h1>
             <Container>
                 <Form>
                     <Form.Group className="mb-3">
@@ -18,7 +23,7 @@ const ListaTurnos = () => {
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Seleccione la fecha</Form.Label>
-                        <DatePicker dateFormat="dd/MMM/yyyy"
+                        <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)} dateFormat="dd/MM/yyyy" placeholderText='25/01/2022' minDate={new Date()} filterDate={date => date.getDay() !== 6 && date.getDay() !== 0} isClearable  isClearable showYearDropdown scrollableMonthYearDropdown locale={es}
                         />
                     </Form.Group>
                     <Form.Group className='mb-3'>
