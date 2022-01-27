687b19590eb0b2966fc73183e15cb44650fd6ece
marcos medina <marcosjmedina@gmail.com>
2022-01-27 00:16:40 -0300

    correcciones por push mal hecho

--- src/Components/PromosPrincipal.js
+++ src/Components/PromosPrincipal.js
@@ -8,58 +8,6 @@ import pizza from "../assets/img/pizza.jpg";
 const PromosPrincipal = () => {
   return (
     <div>
-      <Container fluid className="my-4 ">
-        <CardGroup className="fondoGeneral">
-          <Card className="cardsPromo mx-4">
-            <Card.Img variant="top" src={pizza} />
-            <Card.Body>
-              <Card.Title>PROMO 1</Card.Title>
-              <Card.Text>
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card className="cardsPromo mx-4">
-            <Card.Img variant="top" src={pizza} />
-            <Card.Body>
-              <Card.Title>PROMO 2</Card.Title>
-              <Card.Text>
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card >
-          <Card className="cardsPromo mx-4">
-            <Card.Img variant="top" src={pizza} />
-            <Card.Body>
-              <Card.Title>PROMO 3</Card.Title>
-              <Card.Text>
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card >
-          <Card className="cardsPromo mx-4">
-            <Card.Img variant="top" src={pizza} />
-            <Card.Body>
-              <Card.Title>PROMO 4</Card.Title>
-              <Card.Text>
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardGroup>
-      </Container>
       <Container fluid className="my-4 fondoGeneral">
         <h1 className="text-center py-2">NUESTRAS PROMOS</h1><hr />
         <CardGroup >
