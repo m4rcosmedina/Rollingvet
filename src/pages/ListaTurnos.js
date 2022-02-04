6c4246d74692c87877d00435ffee9a8a309f4371
Julieta <julietahurtadocabbad@gmail.com>
2022-02-04 17:42:47 -0300

    se creo la carpeta CrudListaTurnos para trabajar con el crud

--- src/pages/ListaTurnos.js
+++ src/pages/ListaTurnos.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
+import CrudListaTurnos from '../Components/CrudListaTurnos';
+import { useState } from 'react';
 import { Form, Container, Button, Row, Col } from 'react-bootstrap';
 import DatePicker from "react-datepicker";
 import TimePicker from 'react-time-picker';
@@ -7,63 +9,22 @@ import "react-datepicker/dist/react-datepicker.css";
 import "react-time-picker/dist/TimePicker.css";
 import "react-clock/dist/Clock.css";
 
-const initialForm = {
-    profesional: "",
-    opcion1: "",
-    opcion2: "",
-    consulta: ""
-}
-
-const initialDb = [];
-
 const ListaTurnos = () => {
     // variable para DatePicker
     const [selectedDate, setSelectedDate] = useState(null)
-    // variables del CRUD
-    const [Db, setDb] = useState([initialDb]);
-    const [form, setForm] = useState([initialForm]);
-    // variable de estado
-    const [dataParaEditar, setDataParaEditar] = useState([null]);
-
-    // funciones del CRUD
-    const createData = (data) => { };
-    const updateData = (data) => { };
-    const deleteData = (data) => { };
-
-    const handleChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.profesional]: e.target.value,
-            [e.target.opcion1]: e.target.value,
-            [e.target.opcion2]: e.target.value,
-            [e.target.consulta]: e.target.value
-        });
-     }
-    const handleSubmit = (e) => { 
-        e.preventDefault();
-
-        if(!form.profesional){
-            alert("Debe seleccionar una opcion");
-            return;
-        }; 
-        if (form.id === null){
-            createData(form)
-        }
-
-    }
-    const handleReset = (e) => { }
 
     return (
         <div className="fondoGeneral">
             <h1 className='text-center py-4'>TURNOS</h1>
+            <CrudListaTurnos></CrudListaTurnos>
             <Container>
                 <Row>
                     <Col sm={6}>
-                        <Form createData={createData} updateData={updateData} dataParaEditar={dataParaEditar} setDataParaEditar={setDataParaEditar} onSubmit={handleSubmit}>
+                        <Form>
                             <Form.Select className="mb-3">
-                                <option onChange={handleChange} value={form.profesional}>Seleccione un profesional</option>
-                                <option onChange={handleChange} value={form.opcion1}> Dr. Milagro</option>
-                                <option onChange={handleChange} value={form.opcion2}>Dr. Robazan</option>
+                                <option >Seleccione un profesional</option>
+                                <option > Dr. Milagro</option>
+                                <option >Dr. Robazan</option>
                             </Form.Select>
                             <Form.Group className="mb-3">
                                 <Form.Label>Fecha</Form.Label>
@@ -79,34 +40,27 @@ const ListaTurnos = () => {
                                     label="Perro"
                                     name="formHorizontalRadios"
                                     id="formHorizontalRadios1"
-                                    onChange={handleChange}
-                                    value={form.animal1}
                                 />
                                 <Form.Check
                                     type="radio"
                                     label="Gato"
                                     name="formHorizontalRadios"
                                     id="formHorizontalRadios2"
-                                    onChange={handleChange}
-                                    value={form.animal2}
                                 />
                                 <Form.Check
                                     type="radio"
                                     label="Otro"
                                     name="formHorizontalRadios"
                                     id="formHorizontalRadios3"
-                                    onChange={handleChange}
-                                    value={form.animal3}
                                 />
                             </Form>
                             <Form.Group className="mb-3">
                                 <Form.Label>Seleccione el motivo de su consulta</Form.Label>
-                                <Form.Control type="textarea" onChange={handleChange} value={form.consulta} />
+                                <Form.Control type="textarea" />
                             </Form.Group>
                             <Button type="submit" className="botonEnviar">
                                 Enviar
                             </Button>
-                            <Button type="reset" className="botonEnviar" onClick={handleReset}>Resetear</Button>
                         </Form>
                     </Col>
                 </Row>
