94edd8dc3887f147c2d56f6403410277bd5f36e3
Julieta <julietahurtadocabbad@gmail.com>
2022-02-04 03:29:51 -0300

    se agrego la api y se la encripto

--- src/pages/ListaTurnos.js
+++ src/pages/ListaTurnos.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Container, Button } from 'react-bootstrap';
+import { Form, Container, Button, Row, Col } from 'react-bootstrap';
 import DatePicker from "react-datepicker";
 import TimePicker from 'react-time-picker';
 import es from 'date-fns/locale/es';
@@ -10,41 +10,44 @@ import "react-clock/dist/Clock.css";
 const ListaTurnos = () => {
     const [selectedDate, setSelectedDate] = useState(null)
 
-
     return (
         <div className="fondoGeneral">
             <h1 className='text-center py-4'>TURNOS</h1>
             <Container>
-                <Form>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Seleccione un profesional</Form.Label>
-                        <Form.Select required>
-                            <option>Dr. Milagro</option>
-                            <option>Dr. NoMilagro</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Fecha</Form.Label>
-                        <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)} dateFormat="dd/MM/yyyy" placeholderText='25/01/2022' minDate={new Date()} filterDate={date => date.getDay() !== 6 && date.getDay() !== 0} showYearDropdown scrollableMonthYearDropdown locale={es} required
-                        />
-                    </Form.Group>
-                    <Form.Group className='mb-3'>
-                        <Form.Label>Hora</Form.Label>
-                        <TimePicker className="ms-2" hourPlaceholder='02' minutePlaceholder='50' required />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">Seleccione un animal
-                        <Form.Check type="checkbox" label="Gato" />
-                        <Form.Check type="checkbox" label="Perro" />
-                        <Form.Check type="checkbox" label="Otro" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Seleccione el motivo de su consulta</Form.Label>
-                        <Form.Control type="textarea" />
-                    </Form.Group>
-                    <Button type="submit" className="botonEnviar">
-                        Enviar
-                    </Button>
-                </Form>
+                <Row>
+                    <Col sm={6}>
+                        <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Seleccione un profesional</Form.Label>
+                                <Form.Select required>
+                                    <option>Dr. Milagro</option>
+                                    <option>Dr. NoMilagro</option>
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Fecha</Form.Label>
+                                <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)} dateFormat="dd/MM/yyyy" placeholderText='25/01/2022' minDate={new Date()} filterDate={date => date.getDay() !== 6 && date.getDay() !== 0} showYearDropdown scrollableMonthYearDropdown locale={es} required
+                                />
+                            </Form.Group>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>Hora</Form.Label>
+                                <TimePicker className="ms-2" hourPlaceholder='02' minutePlaceholder='50' required />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">Seleccione un animal
+                                <Form.Check type="checkbox" label="Gato" />
+                                <Form.Check type="checkbox" label="Perro" />
+                                <Form.Check type="checkbox" label="Otro" />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Seleccione el motivo de su consulta</Form.Label>
+                                <Form.Control type="textarea" />
+                            </Form.Group>
+                            <Button type="submit" className="botonEnviar">
+                                Enviar
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
