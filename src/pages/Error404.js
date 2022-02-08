7163fb445f84fb15bf57d499d87b893ac4548630
Giuliano Carbone <benjacarbone@gmail.com>
2022-02-08 00:24:27 -0300

    se agregaron cambios esteticos

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -6,32 +6,19 @@ import { Container, Row, Col,Nav,Button } from "react-bootstrap";
 
 const Error404 = () => {
   return (
-    <div>
-      <Container fluid>
-        <Row>
-          <Col sm={6} className="ladoIzq text-center">
-            <img
-              src={logoNavbar}
-              className="logoHorizontal"
-              alt="logoHorizontal" />
-
-               <Nav.Link href="/" className="boton404">
-                <Button
-                  type="submit"
-                  variant="outline-light"
-                  className="botonEnviar"
-                >
-                 Volver al Home
-                </Button>
-                </Nav.Link>
-          </Col >
-          <Col sm={6} className="ladoDer py-4">
-            <p className="text-center numero404"> 404</p>
-            <p className="text-center texto404">Página no encontrada</p>
-          </Col>
-        </Row>
-      </Container>
+    <div className="text-center">
+    <div className="pt-2">
+    <Nav.Link href="/" className="navbarInicio">
+    <Button
+    type="submit"
+    variant="outline-primary"
+    className="ms-3">
+VOLVER AL INICIO
+    </Button>
+    </Nav.Link>
     </div>
+ <img src="https://trello.com/1/cards/6201d621ff0c9870cae245f8/attachments/6201d897743c3c66389d1fa5/previews/6201d898743c3c66389d1fce/download/ultima-01.png" alt="" width="35%"  className="movimiento media" />
+  </div>
   );
 };
 
