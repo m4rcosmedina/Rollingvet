4afa9b0c814793ad67c8f7effa7b1b7ee4efe7a0
marcos medina <marcosjmedina@gmail.com>
2022-02-13 20:34:43 -0300

    problema en el console log del edit

--- src/pages/Pacientes/EditarPaciente.js
+++ src/pages/Pacientes/EditarPaciente.js
@@ -1,16 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const EditarPaciente = ({ URL }) => {
   const [paciente, setPaciente] = useState({});
 
-  const { id } = useParams();
-  console.log(`${URL}/${id}`);
+  const {id} = useParams();
+
+  const nombreHumRef = useRef("");
+  const emailRef = useRef("");
+  const telRef = useRef("");
+  const nombreRef = useRef("");
+  const especieRef = useRef("");
+  const razaRef = useRef("");
+  const edadRef = useRef("");
 
   useEffect(async () => {
     try {
-      const res = await fetch(`${URL}/${id}`);
+      const res = await fetch( `${URL}/${id}` );
       const pacienteApi = await res.json();
       setPaciente(pacienteApi);
     } catch (error) {
@@ -18,45 +25,70 @@ const EditarPaciente = ({ URL }) => {
     }
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+        console.log(nombreHumRef.current.value);
+
+    };
+
   return (
     <div>
       <Container className="py-5">
         <div className="d-flex align-items-center justify-content-between">
           <h1>Editar datos del paciente</h1>
-          <hr></hr>
+          <hr />
         </div>
 
-        <Form className="my-5">
+        <Form className="my-5" onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Nombre y Apellido del Humano</Form.Label>
             <Form.Control
               type="text"
               defaultValue={paciente.nombreHum}
+              ref={nombreHumRef}
             ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Email</Form.Label>
-            <Form.Control defaultValue={paciente.email}></Form.Control>
+            <Form.Control
+              defaultValue={paciente.email}
+              ref={emailRef}
+            ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Telefono</Form.Label>
-            <Form.Control defaultValue={paciente.tel}></Form.Control>
+            <Form.Control
+              defaultValue={paciente.tel}
+              ref={telRef}
+            ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Nombre</Form.Label>
-            <Form.Control defaultValue={paciente.nombre}></Form.Control>
+            <Form.Control
+              defaultValue={paciente.nombre}
+              ref={nombreRef}
+            ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Especie</Form.Label>
-            <Form.Control defaultValue={paciente.especie}></Form.Control>
+            <Form.Control
+              defaultValue={paciente.especie}
+              ref={especieRef}
+            ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Raza</Form.Label>
-            <Form.Control defaultValue={paciente.raza}></Form.Control>
+            <Form.Control
+              defaultValue={paciente.raza}
+              ref={razaRef}
+            ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Edad</Form.Label>
-            <Form.Control defaultValue={paciente.edad}></Form.Control>
+            <Form.Control
+              defaultValue={paciente.edad}
+              ref={edadRef}
+            ></Form.Control>
           </Form.Group>
           <div className="text-end py-3" variant="outline-primary">
             <Button>Guardar</Button>
