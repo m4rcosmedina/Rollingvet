809056ed1dccee56f5e6bb2b36cc8699fbf18933
marcos medina <marcosjmedina@gmail.com>
2022-02-23 23:10:04 -0300

    pagina planes

--- src/pages/Planes.js
+++ src/pages/Planes.js
@@ -2,13 +2,13 @@ import React, { useRef } from "react";
 import "../CSS/App.css";
 import {
   Form,
-
   InputGroup,
   FormControl,
   Button,
   Container,
   Row,
   Col,
+  Card,
 } from "react-bootstrap";
 import emailjs from "@emailjs/browser";
 
@@ -37,14 +37,14 @@ const Planes = () => {
   return (
     <div>
       <div className="fondoGeneral">
-        <h1 className="text-center py-4"> PLANES</h1>
+        <h1 className="text-center py-3"> PLANES</h1>
         <hr />
         <Container>
           <Row>
             <Col sm={6}>
               <Form ref={form} onSubmit={sendEmail}>
                 {" "}
-                <Form.Label>Ingrese su nombre y apellido</Form.Label>
+                <Form.Label className="py-3"><h4>Ingrese su nombre y apellido </h4></Form.Label>
                 <InputGroup className="mb-3">
                   <FormControl
                     placeholder="Ej:Cosme Fulanito"
@@ -56,7 +56,7 @@ const Planes = () => {
                   />
                 </InputGroup>
                 <Form.Group className="mb-3">
-                  <Form.Label>Ingrese su correo electronico</Form.Label>
+                  <Form.Label className="py-3"><h4>Ingrese su correo electronico </h4></Form.Label>
                   <Form.Control
                     name="email"
                     type="email"
@@ -66,7 +66,7 @@ const Planes = () => {
                     Jamás compartiremos su correo con nadie mas.
                   </Form.Text>
                 </Form.Group>
-                <Form.Label>Teléfono</Form.Label>
+                <Form.Label className="py-3"> <h4>Teléfono</h4></Form.Label>
                 <InputGroup className="mb-3">
                   <FormControl
                     placeholder="Ej:(381)6719988"
@@ -81,19 +81,40 @@ const Planes = () => {
                   className="mb-3"
                   controlId="exampleForm.ControlTextarea1"
                 >
-                  <Form.Label>
-                    Indique el plan sobre el cual desea información.
+                  <Form.Label className="py-3">
+                   <h4> Indique el plan sobre el cual desea información.</h4>
                   </Form.Label>
-                  <Form.Control as="textarea" rows={3} />
+                  <Form.Control as="textarea" rows={7} />
                 </Form.Group>
                 <Form.Group
-                  className="mb-3"
+                  className="mb-3 py-3"
                   controlId="formBasicCheckbox"
                 ></Form.Group>
-                <Button type="submit" className="botonEnviar" value="Send">
+                <Button type="submit" variant="outline-primary"className="botonEnviar" value="Send">
                   Enviar
                 </Button>
               </Form>
+            </Col>
+
+            <Col sm={6}>
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src="https://trello.com/1/cards/61f2fe6cd74cc3228c316da2/attachments/61fec42abb43ab8aee460b6e/download/especialistas-01.png"
+                  className="movimiento "
+                />
+                <Card.Img
+                  variant="top"
+                  src="https://trello.com/1/cards/61f2fe6cd74cc3228c316da2/attachments/61feba7aeb1f718730b8575e/previews/61feba7beb1f718730b85778/download/promo2-01.png"
+                  className="movimiento "
+                />
+                <Card.Img
+                  variant="top"
+                  src="https://trello.com/1/cards/61f2fe6cd74cc3228c316da2/attachments/61feba7c8ef2097dc6e8f167/previews/61feba7d8ef2097dc6e8f17a/download/promo3-01.png"
+                  className="movimiento "
+                />
+                <Card.Body className="text-center py-2"></Card.Body>
+              </Card>
             </Col>
           </Row>
         </Container>
